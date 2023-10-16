43988ec851710efa3d45e09a8d85ccc5ce81ea71
Alvaro Aguinaga <alvarodaniel2808@gmail.com>
2023-10-15 20:45:42 -0500

    chore: initial code

---
+++ src/test/java/org/redis/manager/rediscache/RedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redis.manager.rediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class RedisCacheApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
